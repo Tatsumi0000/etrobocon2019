0670ba1da3876324d98c377c3a3404478a4bbf71
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-06-12 19:25:02 +0900

    Fix

--- src/module/EtRobocon2019.cpp
+++ src/module/EtRobocon2019.cpp
@@ -2,10 +2,22 @@
 #include "Controller.h"
 #include "Calibrator.h"
 #include "Display.h"
+#include "NormalCourse.h"
 
-void EtRobocon2019::start() {
-	Controller controller;
-	Calibrator calibrator(controller);
+void EtRobocon2019::start()
+{
+  Controller controller;
+  Calibrator calibrator(controller);
+  calibrator.calibration();
 
-	calibrator.calibration();
+  // コースと、カラーセンサー目標値の設定
+  bool isRightCourse = calibrator.isLeftCource();
+  int targetBrightness = (calibrator.getWhiteBrightness() + calibrator.getBlackBrightness()) / 2;
+
+  // 右ボタンが押されるまで待つ
+  while(!controller.buttonIsPressedRight()) {
+  }
+
+  NormalCourse normalCourse(controller, targetBrightness);
+  normalCourse.selectedEdgeLR(isRightCourse);
 }
