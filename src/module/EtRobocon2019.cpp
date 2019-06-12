1b46b91892fb308279a897245fce807604af3862
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-06-12 19:37:20 +0900

    Add: コメントの追加

--- src/module/EtRobocon2019.cpp
+++ src/module/EtRobocon2019.cpp
@@ -8,16 +8,19 @@ void EtRobocon2019::start()
 {
   Controller controller;
   Calibrator calibrator(controller);
-  calibrator.calibration();
+  // キャリブレーションする．
+	calibrator.calibration();
 
   // コースと、カラーセンサー目標値の設定
   bool isRightCourse = calibrator.isLeftCource();
+  // 黒と白を足して2で割る．
   int targetBrightness = (calibrator.getWhiteBrightness() + calibrator.getBlackBrightness()) / 2;
 
-  // 右ボタンが押されるまで待つ
+  // 右ボタンが押されるまで待つ（これを書かないと自動で走り出す．）
   while(!controller.buttonIsPressedRight()) {
   }
 
   NormalCourse normalCourse(controller, targetBrightness);
+  // エッジを指定する．（すると走り出す！！）
   normalCourse.selectedEdgeLR(isRightCourse);
 }
