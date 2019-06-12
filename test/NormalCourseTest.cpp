6ef756e595d637485507d0795ac023fbf99c87a5
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-06-12 12:20:13 +0900

    Fix: 場所の変更

---
+++ test/NormalCourseTest.cpp
@@ -0,0 +1,12 @@
+/**
+ *  @file   NormalCourseTest.cpp
+ *  @brief  NormalCourseクラスのテストファイル
+ *  @author Tatsumi0000
+ */
+
+#include "NormalCourse.h"
+#include <gtest/gtest.h>
+
+namespace etrobocon2019_test {
+  
+}
